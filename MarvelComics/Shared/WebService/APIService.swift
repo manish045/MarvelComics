cbf41ffac55ca96fd22b03a175438bceaffcfb46
manish045 <manish@canbrand.in>
2022-05-20 19:44:50 +0400

    add alamofire and session manager for api calls, added endpoints

---
+++ MarvelComics/Shared/WebService/APIService.swift
@@ -0,0 +1,91 @@
+//
+//  APIService.swift
+//  Marvel Comics
+//
+//  Created by Manish Tamta on 20/05/2022.
+//
+
+import Foundation
+import Alamofire
+import UIKit
+
+enum APIResult<T, APIError> {
+    case success(T)
+    case error(APIError)
+}
+
+protocol PerformRequest {
+    func performRequest<T: BaseModel>(endPoint: EndPoints,
+                                      parameters: [String : Any],
+                                      completion: @escaping (APIResult<T, APIError>) -> Void)
+}
+
+class APIService: SessionManager, PerformRequest {
+    
+    static let shared = APIService()
+    var network: Network
+
+    init(network: Network = Network()) {
+        self.network = network
+        super.init()
+        session.configuration.httpAdditionalHeaders?.updateValue(["application/json", "text/html", "image/png", "image/jpeg"], forKey: "Content-Type")
+    }
+    
+    struct ErrorObject: Decodable {
+        let message: String
+    }
+    
+    fileprivate func validateAF<T: BaseModel>(response: DataResponse<T>) -> (successModel: T?, error: APIError?) {
+        
+        switch response.result {
+        case .success(let parsedModel):
+            return (parsedModel, nil)
+        case .failure(let error):
+            print(error)
+            // incase of logout we will check only status code
+            if let data = response.data {
+                do {
+                    let json = try JSONDecoder().decode(ErrorObject.self, from: data)
+                    return (nil, .serverError(json.message))
+                } catch {
+                    return (nil, .parsingFailed)
+                }
+            }
+            return (nil, .serverError(error.localizedDescription))
+        }
+    }
+    
+    func performRequest<T: BaseModel>(endPoint: EndPoints,
+                                      parameters: [String : Any],
+                                      completion: @escaping (APIResult<T, APIError>) -> Void) {
+
+        guard isNetwork() else {
+            completion(.error(APIError.noNetwork))
+            return
+        }
+        
+        let urlStr = APIService.URL(endPoint)
+        let url = Foundation.URL(string: urlStr)!
+        var request = URLRequest(url: url)
+        request.httpMethod = endPoint.httpMethod.rawValue
+        request.cachePolicy = .returnCacheDataDontLoad
+
+        self.request(request).validate().debugLog().responseDecodable { [weak self] (response: DataResponse<T>) in
+            let parsedResponse = self?.validateAF(response: response)
+            guard let object = parsedResponse?.successModel else {
+                completion(.error(parsedResponse?.error ?? .parsingFailed))
+                return
+            }
+            completion(.success(object))
+        }
+    }
+}
+
+extension Request {
+    public func debugLog() -> Self {
+        #if DEBUG
+        debugPrint(self)
+        #endif
+        return self
+    }
+}
